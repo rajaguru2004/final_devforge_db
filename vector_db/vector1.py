9b3e830ae167c9de5841a72c187f1d2e726fda9d
tarakeshp07 <tarakeshman@gmail.com>
2025-11-28 00:44:54 +0530

    Added mmr and added encoded

---
+++ vector_db/vector1.py
@@ -0,0 +1,57 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
+load_dotenv()
+
+# Define the persistent directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+db_dir = os.path.join(current_dir, "db")
+persistent_directory = os.path.join(db_dir, "chroma_db_with_metadata")
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+# Load the existing vector store with the embedding function
+db = Chroma(persist_directory=persistent_directory,
+            embedding_function=embeddings)
+
+
+# Function to query a vector store with different search types and parameters
+def query_vector_store(
+    store_name, query, embedding_function, search_type, search_kwargs
+):
+    if os.path.exists(persistent_directory):
+        print(f"\n--- Querying the Vector Store {store_name} ---")
+        db = Chroma(
+            persist_directory=persistent_directory,
+            embedding_function=embedding_function,
+        )
+        retriever = db.as_retriever(
+            search_type=search_type,
+            search_kwargs=search_kwargs,
+        )
+        relevant_docs = retriever.invoke(query)
+        # Display the relevant results with metadata
+        print(f"\n--- Relevant Documents for {store_name} ---")
+        for i, doc in enumerate(relevant_docs, 1):
+            print(f"Document {i}:\n{doc.page_content}\n")
+            if doc.metadata:
+                print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
+    else:
+        print(f"Vector store {store_name} does not exist.")
+
+
+# Define the user's question
+query = "How did Juliet die?"
+
+print("\n--- Using Max Marginal Relevance (MMR) ---")
+query_vector_store(
+    "chroma_db_with_metadata",
+    query,
+    embeddings,
+    "mmr",
+    {"k": 3, "fetch_k": 20, "lambda_mult": 0.5},
+)
